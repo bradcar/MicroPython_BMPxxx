995a72e0630ab94ebd813e72155ef02a67f714ec
bradcar <bc@bradcarlile.com>
2024-11-17 20:04:22 -0800

    fixed typos

--- examples/bmp581_test.py
+++ examples/bmp581_test.py
@@ -38,19 +38,19 @@ while True:
     sea_level_pressure = bmp.sea_level_pressure
     print(f"Sea level pressure = {sea_level_pressure:.2f} hPa")
     
-    # Pressure in hPA measured at sensor
-    print(f"Sensor pressure = {bmp.pressure:.2f} hPa")
+    # Pressure in hPA measured at sensor, temperature in Celsius
+    pressure = bmp.pressure
+    print(f"Sensor pressure = {pressure:.2f} hPa")
     temp = bmp.temperature
     print(f"temp = {temp:.2f} C")
   
+    # Pressure in hPA measured at sensor
     meters = bmp.altitude
     print(f"Altitude = {meters:.2f} meters")
     feet = meters * 3.28084
     feet_only = int(feet)
     inches = (feet - feet_only) * 12
     print(f"Altitude = {feet_only} feet {inches:.1f} inches\n")
-
-
 
     time.sleep(2.5)
 
